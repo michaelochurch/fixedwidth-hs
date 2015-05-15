128373f4f74bc7a0b1c56780b1c809f8ee3701b5
Michael Church <michael.o.church@gmail.com>
2015-05-15 13:21:54 -0500

    Delete a bunch of unnecessary code.

--- Data/FixedWidth/LargeFile.hs
+++ Data/FixedWidth/LargeFile.hs
@@ -1,36 +1,14 @@
 module Data.FixedWidth.LargeFile where
 
-import Control.Applicative
-import Data.Attoparsec.Text as StrictParse
-import Data.Attoparsec.Text.Lazy as LazyParse
-import qualified Data.Text as Strict
-import qualified Data.Text.Lazy as Lazy
-import qualified Data.ByteString.Lazy.Char8 as LazyBytes
-import Data.Text.Lazy.Encoding (decodeUtf8)
+import qualified Data.Text as StrictText
+import qualified Data.Text.Lazy as LazyText
+import qualified Data.Text.Lazy.IO as LazyTextIO
+import System.IO (Handle, IOMode(..), openFile)
 
-fileToLazyText :: String -> IO Lazy.Text
-fileToLazyText filename = fmap decodeUtf8 $ LazyBytes.readFile filename
+hGetLines :: Handle -> IO [StrictText.Text]
+hGetLines handle = do
+  lazyText <- LazyTextIO.hGetContents handle
+  return $ map LazyText.toStrict (LazyText.lines lazyText)
 
-strictLineNL :: Parser Strict.Text
-strictLineNL = (StrictParse.takeTill isEndOfLine) <* endOfLine
-
-strictLine :: Parser Strict.Text
-strictLine = strictLineNL <|> StrictParse.takeText
-
-chunkLazyText :: Parser Strict.Text -> Lazy.Text -> ([Strict.Text], Maybe String)
-chunkLazyText chunkParser lazyText =
-  if Lazy.null lazyText
-  then ([], Nothing)
-  else
-    case (LazyParse.parse chunkParser lazyText) of
-     LazyParse.Done rest' chunk ->
-       let (chunks, failOpt) = chunkLazyText chunkParser rest'
-       in (chunk : chunks, failOpt)
-     failure@(LazyParse.Fail _ _ _) -> ([], Just $ show failure)
-
--- WARNING: You must discard the 2nd argument if you only intend to
--- parse part of the file.
-chunkFile :: String -> (Parser Strict.Text) -> IO ([Strict.Text], Maybe String)
-chunkFile filename parser = do
-  lazyText <- fileToLazyText filename
-  return $ chunkLazyText parser lazyText  
+getLines :: String -> IO [StrictText.Text]
+getLines filename = openFile filename ReadMode >>= hGetLines
--- Main.hs
+++ Main.hs
@@ -3,19 +3,24 @@ import Data.Aeson
 import qualified Data.Attoparsec.Text as Parse
 import qualified Data.ByteString.Lazy.Char8 as ByteString
 import Data.FixedWidth.Examples
-import Data.FixedWidth.LargeFile (chunkFile, strictLine)
+import Data.FixedWidth.LargeFile
 import qualified Data.Text as Strict
 import System.Environment (getArgs)
+
+-- TODO: the Entry-related stuff belongs in (a/) separate testing
+-- module(s).
 
 getEntry :: Strict.Text -> Entry
 getEntry text =
   case (Parse.parseOnly parseEntry text) of
-    Right entry -> entry
+    Right entry   -> entry
     Left  _       -> error "shouldn't happen"
 
+-- Right now, the function of this main is to verify the absence of a
+-- memory leak in LargeFile.
 main :: IO ()
 main = do
   args <- getArgs
-  when (length args == 0) $ error "usage: \"quickparse\" <filename>"
-  (fileLines, _) <- chunkFile (args !! 0) strictLine
-  mapM_ (ByteString.putStrLn . encode . getEntry) fileLines
+  when (length args == 0) $ error "this program requires a filename"
+  let filename = args !! 0
+  fmap length (getLines filename) >>= print
